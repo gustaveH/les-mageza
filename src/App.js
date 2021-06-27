3621a91e235fe96676fc63126b5443681052a1bd
gustaveH <ghabiman@gmail.com>
2021-06-27 14:06:44 -0400

    logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import SaveTheDate from './components/SaveDate';
+import Card from '../src/image/card1.png';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <SaveTheDate />
-    </div>
+    <>
+      <Helmet>
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='Les Mageza' />
+        <meta
+          name='twitter:description'
+          content='Were Getting Married! Save the date!'
+        />
+        <meta name='twitter:image' content={Card} />
+      </Helmet>
+      <div>
+        <SaveTheDate />
+      </div>
+    </>
   );
 }
 
